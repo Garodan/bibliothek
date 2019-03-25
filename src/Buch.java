e2b0b8c26bee48d0b787dc58cb6e40893d058343
Garodan <can.h.kalkan@gmail.com>
2019-03-26 00:39:28 +0100

    Update Buch.java

--- src/Buch.java
+++ src/Buch.java
@@ -1,4 +1,13 @@
+//Ein Buch hat einen Autor, Titel und Nummer. //check
+//Außerdem hat es die Eigenschaft, ob es ausleihbar ist. //check
+//(Einige Bücher dürfen nur in der Bibliothek genutzt werden.)
+//Die Ausleihe kann verlängert werden.
+//Beim Ausleihen wird ein Ausleihobjekt erzeugt(Buch, Nutzer, Rückgabedatum).
+//Die Daten des Buches können ausgegeben werden, dazu ist die Buchnummer erforderlich.
 
 public class Buch {
-
+	String autor;
+	String titel;
+	int nummer;
+	boolean ausleihbar;
 }
