a9dcc5fc80bd3c52da81be43e196ad75c610747c
Garodan <can.h.kalkan@gmail.com>
2019-03-25 22:04:06 +0100

    Klassen und Aufgaben
    Anlegen der Klassen und Aufgaben in Kommentaren

---
+++ src/Ausweis.java
@@ -0,0 +1,5 @@
+
+public class Ausweis {
+//Ein Ausweis ist 1 Jahr gültig. Er wird automatisch gesperrt, wenn der maximal erlaubte Gebührenstand überschritten ist oder die Laufzeit beendet ist.
+//Die Verlängerung des Ausweises oder das Entsperren wird in der Anwedung nicht implementiert.
+}
